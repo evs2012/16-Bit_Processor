ef9912b0ea9c2db9b6d5db53a1ca39da48e361b3
evs2012 <evanmweiss12@gmail.com>
2016-12-01 10:52:03 -0500

    It compiles, not tested

--- Processor/Processor/src/InstructionMemory.vhd
+++ Processor/Processor/src/InstructionMemory.vhd
@@ -1,20 +1,44 @@
 -- Instruction memory
 -- May be implemented behaiorally using arrays
 
--- instructions are as follows:	
--- hex  -- instruction
--- 500A -- ldi $r0, 10
--- 5105 -- ldi $r1, 5
--- 5200 -- ldi $r2, 0
--- 5300 -- ldi $r3, 0
--- 5400 -- ldi $r4, 0
--- 5500 -- ldi $r5, 0
--- 5600 -- ldi $r6, 0
--- 5700 -- ldi $r7, 0
--- 0201 -- add $r2, $r0, $r1
--- 1301 -- mult $r3, $r0, $r1
--- 4401 -- sub $r4, $r0, $r1
--- 630B -- sh $r3, 0x0B
--- 640A -- sh $r4, 0x0A
--- 760A -- lh $r6, 0x0A
--- 770B -- lh $r7, 0x0B
+library ieee ;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+---------------------------------------------
+
+entity InstructionMemory is
+port(	  
+	PC		:	in std_logic_vector(4 downto 0);
+	clk		:	in std_logic; 
+	Data	:	out std_logic_vector(15 downto 0)
+);
+end InstructionMemory;
+
+architecture Behavioral of InstructionMemory is
+begin
+	Process(clk)
+		type t_MemArray is array (14 downto 0)
+		of std_logic_vector(15 downto 0);
+		constant Mem : t_MemArray :=
+		-- instructions are as follows:	
+		-- hex  -- instruction
+		(x"500A", -- ldi $r0, 10
+		 x"5105", -- ldi $r1, 5
+		 x"5200", -- ldi $r2, 0
+		 x"5300", -- ldi $r3, 0
+		 x"5400", -- ldi $r4, 0
+		 x"5500", -- ldi $r5, 0
+		 x"5600", -- ldi $r6, 0
+		 x"5700", -- ldi $r7, 0
+		 x"0201", -- add $r2, $r0, $r1
+		 x"1301", -- mult $r3, $r0, $r1
+		 x"4401", -- sub $r4, $r0, $r1
+		 x"630B", -- sh $r3, 0x0B
+		 x"640A", -- sh $r4, 0x0A 
+		 x"760A", -- lh $r6, 0x0A
+		 x"770B"); -- lh $r7, 0x0B
+	Begin
+		Data <= Mem(to_integer(unsigned(PC)));
+	End Process;
+end architecture; 
