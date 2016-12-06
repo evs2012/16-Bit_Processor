219a39e9ad2f7355e827ffb9da917c370e932d32
Daniel Sciortino <DanSciortino@users.noreply.github.com>
2016-12-06 11:43:52 -0500

    made 8x16 reg
    This should compile. May have forgot a semicolon somewhere.

--- Processor/Processor/src/RegisterFile.vhd
+++ Processor/Processor/src/RegisterFile.vhd
@@ -1,2 +1,28 @@
---Implement the 8 x 16 bit register file 
--- May be implemented behaiorally using arrays
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+ENTITY reg is
+	PORT(clk, regWrite: IN STD_LOGIC;
+		dataIN: IN UNSIGNED (16 DOWNTO 0);
+		dr, sr1, sr2: IN UNSIGNED (4 DOWNTO 0);
+		readReg1, readReg2: OUT (16 DOWNTO 0)
+		);
+END ENTITY reg;
+
+ARCHITECTURE behavioral OF reg IS
+	TYPE ram IS ARRAY (0 TO 8) OF UNSIGNED (16 DOWNTO 0);
+	SIGNAL regs: ram := (OTHERS => (OTHERS=> '0'));
+
+BEGIN
+	PROCESS(clk)
+		BEGIN
+			IF clk'EVENT AND clk = '1' THEN
+				IF regWrite = '1' THEN
+					regs(TO_INTEGER(dr)) <= dataIN;
+				END IF;
+			END IF;
+		END PROCESS;
+	readReg1 <= regs(TO_INTEGER(sr1));
+	readReg2 <= regs(TO_INTEGER(sr2));
+END behavioral;
